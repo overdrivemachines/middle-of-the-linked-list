ec2168bc8b943f89cb74bd7461eabd70eb246aeb
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-03 11:26:03 -0700

    added print and create functions

--- middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* createLinkedList(vector<int> values);
+void printLinkedList(ListNode* head);
+
+int main(int argc, char const* argv[]) {
   return 0;
+}
+
+// Create Linked list given a vector of int values
+// returns pointer to head of lined list
+ListNode* createLinkedList(vector<int> values) {
+  ListNode* head;
+  ListNode* node = new ListNode();
+  head = node;
+  for (int value : values) {
+    node->next = new ListNode(value);
+    node = node->next;
+  }
+  head = head->next;
+  return head;
+}
+
+// Prints Linked List
+// e.g. 5->8->3->NULL
+void printLinkedList(ListNode* head) {
+  ListNode* node = head;
+
+  while (node != nullptr) {
+    cout << node->val << "->";
+    node = node->next;
+  }
+  cout << "NULL\n";
 }
